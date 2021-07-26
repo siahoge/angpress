85367bd98de0c2b105b2cc8b90f290877daab502
sia <sia.hoge@gmail.com>
2021-07-26 22:48:52 +0900

    add files

---
+++ cypress/integration/spec.ts
@@ -0,0 +1,5 @@
+it('loads examples', () => {
+  cy.visit('/');
+
+  cy.get('input').click();
+});
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angpress';
+
+  onClick(): void {
+    throw Error('error!!!');
+  }
+}
